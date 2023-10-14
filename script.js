b49b744a3953af2bdd0f42624164be6b19444763
Felix Fleku <99708974+anklet1@users.noreply.github.com>
2023-10-14 11:52:47 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    for (const link of links) {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const yOffset = -80; 
+                const y = targetElement.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+                window.scrollTo({ top: y, behavior: 'smooth' });
+            }
+        });
+    }
+});
